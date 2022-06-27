06a9889d5f8cda771f4b5e2d64a20a62b21860fb
kolo3133 <jakubkolodziejski3105@gmail.com>
2022-06-27 17:45:01 +0200

    Added controls

--- input.js
+++ input.js
@@ -1,3 +1,26 @@
-let inputDirection = { x: 0, y: 0 }; // setting to not move a snake by default
+export let inputDirection = { x: 0, y: 0 }; // setting to not move a snake by default
 
-export const getInputDirection = () => {};
+window.addEventListener("keydown", (e) => {
+  //Modifying snake position by this switch statement
+  switch (
+    e.key // taking key as a event
+  ) {
+    case "ArrowUp":
+      inputDirection = { x: 0, y: -1 };
+      break;
+    case "ArrowDown":
+      inputDirection = { x: 0, y: 1 };
+      break;
+    case "ArrowLeft":
+      inputDirection = { x: -1, y: 0 };
+      break;
+    case "ArrowRight":
+      inputDirection = { x: 1, y: 0 };
+      break;
+  }
+});
+
+export function getInputDirection() {
+  lastInputDirection = inputDirection;
+  return inputDirection;
+}
--- snake.js
+++ snake.js
@@ -1,19 +1,17 @@
-import { getInputDirection } from "./input.js";
+import { getInputDirection, inputDirection } from "./input.js";
 
 export const SNAKE_SPEED = 1; //how many times snake is going to move per second
 const snakeBody = [{ x: 11, y: 11 }]; // setting position for snake
 
 // fucntion for updating all game logic
 export function update() {
-  const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
 
     // Defines that position of snake is based on inputDirection varible
-
-    snakeBody[0].x += inputDirection.x;
-    snakeBody[0].y += inputDirection.y;
   }
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
 }
 
 // function that draws everything on a screen based on update function
